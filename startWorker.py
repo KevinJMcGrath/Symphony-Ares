900ff8bfda0d6cd54a1f89472db35371042ed270
Kevin McGrath <kevin.mcgrath@symphony.com>
2017-09-04 12:36:37 -0400

    Changed worker to use Redis config from config.json

--- startWorker.py
+++ startWorker.py
@@ -1,18 +1,18 @@
-import os
-
 import redis
 from rq import Worker, Queue, Connection
+
+import modules.botconfig as config
 
 listen = ['high', 'default', 'low']
 
 conn = redis.StrictRedis(
-	host='localhost',
-	port=6379,
-	password='suzumebachi')
+    host=config.RedisHost,
+    port=config.RedisPort,
+    password=config.RedisPassword)
 
 conn.ping()
 
 if __name__ == '__main__':
-	with Connection(conn):
-		worker = Worker(map(Queue, listen))
-		worker.work()
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
