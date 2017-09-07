6d7c9052d57c95f2c41679853af6e65e935fbb8d
kevin mcgrath <kevin.mcgrath@symphony.com>
2017-09-07 18:24:25 -0400

    Changed from PyCrypto (no longer developed) to cryptography.io

--- modules/crypto.py
+++ modules/crypto.py
@@ -1,13 +1,21 @@
-from Crypto.Hash import SHA256
+# from Crypto.Hash import SHA256
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
 
 def GetHash(inputStr):
-    newHash = SHA256.new()
+    # newHash = SHA256.new()
+    newHash = hashes.Hash(hashes.SHA256(), backend=default_backend())
     newHash.update(inputStr.encode('utf-8'))
-    return newHash
+
+    hashBytes = newHash.finalize()
+
+
+    return newHash.hex()
 
 
 def CompareHashDigest(cleartext, compareToHash):
-    newHash = SHA256.new(cleartext.encode('utf-8'))
+    # newHash = SHA256.new(cleartext.encode('utf-8'))
 
-    return newHash.hexdigest() == compareToHash
+    return GetHash(cleartext) == compareToHash
